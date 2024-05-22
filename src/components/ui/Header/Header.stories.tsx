694d54c957db286e773ce298eb0530c76bb15242
Simo Echaarani <echaaranimohamed@gmail.com>
2024-05-22 14:17:05 +0100

    Adding a stories file for Header comp Header.stories.tsx

---
+++ src/components/ui/Header/Header.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Header } from "./";
+import { type MenuItemT } from "../Dropdown";
+
+function clickHandler() {
+  return;
+}
+const menuItems: MenuItemT[] = [
+  { label: "Contact info", clickHandler },
+  { label: "Select messages", clickHandler },
+  { label: "Close chat", clickHandler },
+  { label: "Mute notifications", clickHandler },
+  { label: "Disappearing messages", clickHandler },
+  { label: "Clear chat", clickHandler },
+  { label: "Delete chat", clickHandler },
+  { label: "Report", clickHandler },
+  { label: "Block", clickHandler },
+];
+
+const meta = {
+  title: "UI/Header",
+  component: Header,
+  args: {
+    menuItems,
+  },
+  // tags:[""]
+} satisfies Meta<typeof Header>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const WithuserName: Story = {
+  args: {
+    username: "Simo Echaarani",
+  },
+};
