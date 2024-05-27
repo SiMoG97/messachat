e44a95145c9e81c383b2cee2cbfdb38374280ec3
Simo Echaarani <echaaranimohamed@gmail.com>
2024-05-27 17:22:16 +0100

    fetch all current user's conversations

---
+++ src/actions/getCurrentUserConversations.ts
@@ -0,0 +1,22 @@
+import { db } from "@/server/db";
+import { getCurrentUser } from "./getCurrentUser";
+
+export async function getConversations() {
+  const currentUser = await getCurrentUser();
+  if (!currentUser?.id) return [];
+
+  try {
+    const conversations = await db.conversation.findMany({
+      orderBy: { lastMessageAt: "desc" },
+      where: { userIds: { has: currentUser.id } },
+      include: {
+        users: true,
+        messages: { include: { sender: true, seen: true } },
+      },
+    });
+
+    return conversations;
+  } catch (error) {
+    return [];
+  }
+}
